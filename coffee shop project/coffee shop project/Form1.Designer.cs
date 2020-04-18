08407a12fdc5c9c7202b96c5f3c410a1f1d98a37
jjthanawat <04494j@gmail.com>
2020-04-18 14:16:32 +0700

    calculate price3

--- coffee shop project/coffee shop project/Form1.Designer.cs
+++ coffee shop project/coffee shop project/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -55,7 +56,7 @@
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(54, 221);
+            this.listBox1.Location = new System.Drawing.Point(54, 269);
             this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(651, 236);
@@ -83,6 +84,7 @@
             this.comboBox1.Size = new System.Drawing.Size(390, 37);
             this.comboBox1.TabIndex = 4;
             this.comboBox1.Text = "เลือกเครื่องดื่ม";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
@@ -132,29 +134,31 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(724, 221);
+            this.button2.Location = new System.Drawing.Point(724, 269);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(154, 36);
             this.button2.TabIndex = 5;
             this.button2.Text = "ลบเครื่องดื่ม";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(49, 513);
+            this.label2.Location = new System.Drawing.Point(49, 234);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 29);
+            this.label2.Size = new System.Drawing.Size(84, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "รวมราคา";
+            this.label2.Text = "รายการ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(172, 511);
+            this.textBox1.Location = new System.Drawing.Point(172, 546);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -166,7 +170,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(49, 554);
+            this.label3.Location = new System.Drawing.Point(49, 589);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 29);
@@ -176,7 +180,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(172, 553);
+            this.textBox2.Location = new System.Drawing.Point(172, 588);
             this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(215, 35);
@@ -187,7 +191,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(405, 513);
+            this.label4.Location = new System.Drawing.Point(405, 548);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 29);
@@ -198,7 +202,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(405, 554);
+            this.label5.Location = new System.Drawing.Point(405, 589);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(51, 29);
@@ -209,7 +213,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(405, 597);
+            this.label6.Location = new System.Drawing.Point(405, 632);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(51, 29);
@@ -220,7 +224,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(49, 597);
+            this.label7.Location = new System.Drawing.Point(49, 632);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(91, 29);
@@ -230,7 +234,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(172, 596);
+            this.textBox3.Location = new System.Drawing.Point(172, 631);
             this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
@@ -282,6 +286,18 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "ชำระเงิน";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(49, 552);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 29);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "รวมราคา";
+            this.label8.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -306,6 +322,7 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label2);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
@@ -319,7 +336,6 @@
 
         #endregion
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Button button1;
@@ -337,6 +353,8 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- coffee shop project/coffee shop project/Form1.cs
+++ coffee shop project/coffee shop project/Form1.cs
@@ -40,20 +40,7 @@ namespace coffee_shop_project
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-            string sql = "select * from products";
-            MySqlConnection con = new MySqlConnection("host = localhost;user=root;password=123456789;database=py_database");
-
-            MySqlCommand cmd = new MySqlCommand(sql, con);
-            con.Open();
-            MySqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                string product = reader.GetString("ProductName");
-                listBox1.Items.Add(product);
-            }
-            //cmd.ExecuteNonQuery();
-            con.Close();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -61,15 +48,32 @@ namespace coffee_shop_project
             string N_product = comboBox1.Text; //เครื่องดื่ม
             string T_product = comboBox2.Text; //ชนิดเครื่องดื่ม
             string O_product = comboBox3.Text; //เพิ่มเติม
-            if (O_product == "ไม่เพิ่ม")
+            if (N_product == "เลือกเครื่องดื่ม") //เช็คเงื่อนไข เลือกเครื่องดื่มหรือยัง
             {
-                listBox1.Items.Add("" + N_product + " " + T_product + "");
+                MessageBox.Show("เครื่องดื่มยังไม่ถูกเลือก");
             }
             else
             {
+                if (T_product == "ชนิด") //เช็คเงื่อนไข เลือกชนิดเครื่องดื่มหรือยัง
+                {
+                    MessageBox.Show("ชนิดเครื่องดื่มยังไม่ถูกเลือก");
+                }
+                else
+                {   //เช็คเงื่อนไข เพิ่มอะไรลงในเครื่องดื่มหรือไม่
+                    if (O_product == "ไม่เพิ่ม" || O_product == "เพิ่มเติม")   //ไม่เพิ่ม
+                    {
+                        listBox1.Items.Add("" + N_product + " " + T_product + "");
+                    }
+                    else   //เพิ่ม
+                    {
 
-                listBox1.Items.Add("" + N_product + " " + T_product + " " + O_product + "");
+                        listBox1.Items.Add("" + N_product + " " + T_product + " " + O_product + ""); //เพิ่มรายการนี้ลงใน listbox
+                    }
+                }
             }
+
+
+
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -88,6 +92,41 @@ namespace coffee_shop_project
             }
             //cmd.ExecuteNonQuery();
             con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Text == "")
+            {
+                MessageBox.Show("กรุณาเลือกเครื่องดื่มในหน้ารายการ");
+            }
+            else
+            {
+                listBox1.Items.Remove(listBox1.Text);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*listBox1.Items.Clear();
+            string sql = "select * from products";
+            MySqlConnection con = new MySqlConnection("host = localhost;user=root;password=123456789;database=py_database");
+
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            con.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string product = reader.GetString("ProductName");
+                listBox1.Items.Add(product);
+            }
+            //cmd.ExecuteNonQuery();
+            con.Close();*/
         }
     }
 }
