e1a91ad4711513dfe4a040c6d8e343920f381a7c
jjthanawat <04494j@gmail.com>
2020-04-19 11:33:01 +0700

    calculate point1

--- coffee shop project/coffee shop project/Form1.Designer.cs
+++ coffee shop project/coffee shop project/Form1.Designer.cs
@@ -65,6 +65,7 @@
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.label19 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -251,7 +252,7 @@
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.button3.Location = new System.Drawing.Point(197, 38);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(163, 78);
             this.button3.TabIndex = 7;
@@ -263,7 +264,7 @@
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button4.Location = new System.Drawing.Point(27, 38);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(163, 78);
             this.button4.TabIndex = 7;
@@ -275,7 +276,7 @@
             this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button5.Location = new System.Drawing.Point(368, 38);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(163, 78);
             this.button5.TabIndex = 7;
@@ -306,10 +307,10 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(589, 547);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Location = new System.Drawing.Point(589, 686);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(196, 273);
+            this.button6.Size = new System.Drawing.Size(272, 134);
             this.button6.TabIndex = 8;
             this.button6.Text = "ชำระเงิน";
             this.button6.UseVisualStyleBackColor = true;
@@ -421,7 +422,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(847, 570);
+            this.label14.Location = new System.Drawing.Point(913, 600);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(126, 36);
             this.label14.TabIndex = 1;
@@ -432,7 +433,7 @@
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(847, 609);
+            this.label15.Location = new System.Drawing.Point(913, 639);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(113, 36);
             this.label15.TabIndex = 1;
@@ -443,7 +444,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(847, 687);
+            this.label16.Location = new System.Drawing.Point(913, 717);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(193, 36);
             this.label16.TabIndex = 1;
@@ -454,7 +455,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label17.Location = new System.Drawing.Point(847, 726);
+            this.label17.Location = new System.Drawing.Point(913, 756);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(219, 36);
             this.label17.TabIndex = 1;
@@ -465,7 +466,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label18.Location = new System.Drawing.Point(847, 765);
+            this.label18.Location = new System.Drawing.Point(913, 795);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(201, 36);
             this.label18.TabIndex = 1;
@@ -476,12 +477,22 @@
             // 
             this.label19.AutoSize = true;
             this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label19.Location = new System.Drawing.Point(847, 648);
+            this.label19.Location = new System.Drawing.Point(913, 678);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(185, 36);
             this.label19.TabIndex = 1;
             this.label19.Text = "เบอร์โทรลูกค้า";
             this.label19.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(589, 547);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(272, 135);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "สะสมแต้ม";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -489,6 +500,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(1259, 846);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
@@ -572,6 +584,7 @@
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label18;
         private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Button button7;
     }
 }
 
